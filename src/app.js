ccdef99512d8f34e867c15289cfffb6a6a22f787
sliitguy <it23259416@my.sliit.lk>
2025-10-02 15:13:50 +0530

    updated another endpiont

--- src/app.js
+++ src/app.js
@@ -14,6 +14,10 @@ app.use(cors({
 
 app.use(express.json())
 
+app.get('/', (req, res) => {
+    res.json({message: "Sparrow: API Gateway"})
+})
+
 app.get('/health', (req, res) => {
     res.json({message: "API Gateway is running.."})
 })
